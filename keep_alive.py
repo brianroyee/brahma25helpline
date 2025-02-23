e7873da4ecaf3de691b04bf2ab919a7c8014fdae
Ngtfury <sreeramachandransmenon@gmail.com>
2025-02-23 16:06:22 +0530

    Add Flask endpoint to see JSON file.

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 from threading import Thread
 
 app = Flask(__name__)
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return "Running"
+
+@app.route('/data', methods=['GET'])
+def _data():
+  try:
+    with open('data/bot_stats.json', 'r') as f:
+      data = f.read()
+    return jsonify(data)
+  except Exception as e:
+    return jsonify(
+      {
+        "Error": str(e)
+      }
+    )
 
 def run():
   app.run(host='0.0.0.0', port=8080)
